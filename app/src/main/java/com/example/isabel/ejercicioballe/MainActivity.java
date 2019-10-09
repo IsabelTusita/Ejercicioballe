009b03f9e6c13a80f5e31035142d0c7ea831c670
Tusita <isabel@MacBook-Pro-de-isabel.local>
2019-10-09 18:49:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/isabel/ejercicioballe/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.isabel.ejercicioballe;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Cargamos el texto a exportar
+        final EditText textoexportar;
+        textoexportar= findViewById(R.id.pasarTexto);
+
+
+        Button btn = findViewById(R.id.botonact2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), AcercaDe.class);
+                //Exportar parametro
+                intent.putExtra("frase", textoexportar.getText().toString());
+                //
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+}
